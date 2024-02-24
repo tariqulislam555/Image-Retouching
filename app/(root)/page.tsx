624fcaf4ea4ab5c1bf14d368b9fbea7a4271caac
tariqulislam555 <124367172+tariqulislam555@users.noreply.github.com>
2024-02-24 20:40:11 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      Home
+      <UserButton/>
+    </div>
+  )
+}
+
+export default Page
